3c73ba7754773f4e331bba817901ab17a749dab7
Irshad Allam N <allamirshad8@gmail.com>
2024-11-25 17:53:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://qr-client-omgs.onrender.com/",
+    origin: "https://qr-client-omgs.onrender.com",
   })
 );
 app.use(express.json());
